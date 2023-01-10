f4ea3d529265a2707c2f27c4d81266569e37265c
Zsoltbanyai <zsolt.banyai94@gmail.com>
2023-01-10 16:00:08 +0100

    #5 finalize GrannyService

--- grannymanager/src/main/java/com/codecool/grannymanager/service/GrannyService.java
+++ grannymanager/src/main/java/com/codecool/grannymanager/service/GrannyService.java
@@ -5,6 +5,10 @@ import com.codecool.grannymanager.repository.GrannyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Random;
+
 @Service
 public class GrannyService {
     private final GrannyRepository grannyRepository;
@@ -16,22 +20,49 @@ public class GrannyService {
 
     public void visitGranny(int id) {
         Granny granny = grannyRepository.findGrannyById(id);
-        granny.checkOnGranny();
+        LocalDateTime lastVisit = granny.getLastVisit();
+        int hoursSinceLastVisit = (int)ChronoUnit.HOURS.between(lastVisit, LocalDateTime.now());
+        int daysSinceLastVisit = Math.floorDiv(hoursSinceLastVisit, 24);
+        checkOnGranny(granny, daysSinceLastVisit);
+        granny.setLastVisit(LocalDateTime.now());
+    }
+
+    private void checkOnGranny(Granny granny, int daysSinceLastVisit) {
+        List<Stat> grannyStats = granny.getStats();
+        Random random = new Random();
+        while (daysSinceLastVisit > 0) {
+            daysSinceLastVisit--;
+            Stat randomStat = grannyStats.get(random.nextInt(3));
+            randomStat.decrementStat();
+        }
+        shouldGrannyRetire(granny);
+    }
+
+    private void shouldGrannyRetire(Granny granny) {
+        Stat mood = granny.getMood();
+        Stat health = granny.getHealth();
+        Stat environment = granny.getEnvironment();
+        if (mood == Mood.GRUMPY && health == Health.SICK && environment == Environment.IN_RUINS) {
+            granny.setRetired(true);
+        }
     }
 
     public void feedPie(int id) {
         Granny granny = grannyRepository.findGrannyById(id);
-        granny.increaseHealth();
+        Stat health = granny.getHealth();
+        health.increaseStat();
     }
 
     public void playMahjong(int id) {
         Granny granny = grannyRepository.findGrannyById(id);
-        granny.increaseFeeling();
+        Stat mood = granny.getMood();
+        mood.increaseStat();
     }
 
     public void cleanHouse(int id) {
         Granny granny = grannyRepository.findGrannyById(id);
-        granny.increaseEnvironment();
+        Stat environment = granny.getEnvironment();
+        environment.increaseStat();
     }
 
 }
