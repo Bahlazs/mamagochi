3d58a3818f728ab7daa6842895b41d33472c8ae6
bahlazs <nick.balazs18@gmail.com>
2023-01-10 09:21:28 +0100

    add spring boot project

---
+++ grannymanager/src/main/java/com/codecool/grannymanager/GrannymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.grannymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrannymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrannymanagerApplication.class, args);
+	}
+
+}
---
+++ grannymanager/src/test/java/com/codecool/grannymanager/GrannymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.grannymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrannymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
