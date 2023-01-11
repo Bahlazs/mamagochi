97c9101dce75a7bb9110976df835396956c62f44
eszterberenyi <e.eszterberenyi@gmail.com>
2023-01-11 11:23:28 +0100

    change controller to restcontroller annotation in usercontroller

--- grannymanager/src/main/java/com/codecool/grannymanager/controller/UserController.java
+++ grannymanager/src/main/java/com/codecool/grannymanager/controller/UserController.java
@@ -3,13 +3,9 @@ package com.codecool.grannymanager.controller;
 import com.codecool.grannymanager.model.User;
 import com.codecool.grannymanager.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
