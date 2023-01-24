5baeff285c3d0c143aeb20fd96d5cf280918c835
bahlazs <nick.balazs18@gmail.com>
2023-01-24 14:39:21 +0100

    start implementing navbar

---
+++ react/granny-manager-react-app/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import Box from '@mui/material/Box'
+
+const NavBar = () => {
+    return (
+        <Box sx= {{
+            width: 1,
+            height: 50,
+            backGroundColor: "black",
+        }}>
+
+        </Box>
+    )
+}
+
+export default NavBar
