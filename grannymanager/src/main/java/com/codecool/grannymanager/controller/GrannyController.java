9c9770499e74d774b949126774bea28639377924
eszterberenyi <e.eszterberenyi@gmail.com>
2023-01-24 13:32:24 +0100

    remove unused import

--- grannymanager/src/main/java/com/codecool/grannymanager/controller/GrannyController.java
+++ grannymanager/src/main/java/com/codecool/grannymanager/controller/GrannyController.java
@@ -2,7 +2,6 @@ package com.codecool.grannymanager.controller;
 
 
 import com.codecool.grannymanager.model.Granny;
-import com.codecool.grannymanager.model.grannyproperties.Stat;
 import com.codecool.grannymanager.model.requestmodel.GrannyCreateRequest;
 import com.codecool.grannymanager.model.requestmodel.GrannyGetRequest;
 import com.codecool.grannymanager.service.GrannyService;
@@ -10,11 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.codecool.grannymanager.service.GrannyService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
