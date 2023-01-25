0716c22baf095fee5d33e3d0f781926d06829b7e
Zsoltbanyai <zsolt.banyai94@gmail.com>
2023-01-25 18:46:19 +0100

    setup dev proxy

--- grannymanager/src/main/java/com/codecool/grannymanager/controller/GrannyController.java
+++ grannymanager/src/main/java/com/codecool/grannymanager/controller/GrannyController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/granny")
 public class GrannyController {
     private final GrannyService grannyService;
