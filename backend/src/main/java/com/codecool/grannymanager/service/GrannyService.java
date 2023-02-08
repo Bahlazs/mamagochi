45a104ac0a78a6974a2c1ee3b1aac496438fd553
bahlazs <nick.balazs18@gmail.com>
2023-02-08 09:31:32 +0100

    add minor bugfix to decrement stat

--- backend/src/main/java/com/codecool/grannymanager/service/GrannyService.java
+++ backend/src/main/java/com/codecool/grannymanager/service/GrannyService.java
@@ -33,10 +33,8 @@ public class GrannyService {
     public Granny visitGranny(long id) {
         Granny granny = grannyRepository.findGrannyById(id);
         decrementRandomStat(granny);
-        retireGranny(granny);
         granny.setLastVisit(LocalDateTime.now());
         return grannyRepository.save(granny);
-//        return granny;
     }
 
 
@@ -44,34 +42,35 @@ public class GrannyService {
         int statChanged = 0;
         int numberOfDaysSinceLastVisit = extractDaysSinceLastVisit(granny.getLastVisit());
         String[] stats = new String[] {"environment", "health", "mood"};
-        while (statChanged < numberOfDaysSinceLastVisit) {
-            String stat = stats[chooseRandomStatIndex()];
+            while (statChanged < numberOfDaysSinceLastVisit && !granny.isRetired()) {
+                String stat = stats[chooseRandomStatIndex()];
 
-            switch (stat) {
-                case "environment" -> {
-                    int environmentStat = granny.getEnvironmentStat();
-                    if (environmentStat > 1) {
-                        granny.setEnvironmentStat(environmentStat - 1);
-                        statChanged++;
+                switch (stat) {
+                    case "environment" -> {
+                        int environmentStat = granny.getEnvironmentStat();
+                        if (environmentStat > 1) {
+                            granny.setEnvironmentStat(environmentStat - 1);
+                            statChanged++;
+                        }
+                    }
+                    case "health" -> {
+                        int healthStat = granny.getHealthStat();
+                        if (healthStat > 1) {
+                            granny.setHealthStat(healthStat - 1);
+                            statChanged++;
+                        }
+                    }
+                    case "mood" -> {
+                        int moodStat = granny.getMoodStat();
+                        if (moodStat > 1) {
+                            granny.setMoodStat(moodStat - 1);
+                            statChanged++;
+                        }
                     }
                 }
-                case "health" -> {
-                    int healthStat = granny.getHealthStat();
-                    if (healthStat > 1) {
-                        granny.setHealthStat(healthStat - 1);
-                        statChanged++;
-                    }
-                }
-                case "mood" -> {
-                    int moodStat = granny.getMoodStat();
-                    if (moodStat > 1) {
-                        granny.setMoodStat(moodStat - 1);
-                        statChanged++;
-                    }
-                }
+                retireGranny(granny);
             }
         }
-    }
 
     private int chooseRandomStatIndex() {
         Random random = new Random();
